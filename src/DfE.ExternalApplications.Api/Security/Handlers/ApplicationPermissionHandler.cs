63ebe1f0cbf578502b7a86346d225a07b569177e
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-07 14:47:01 +0100

    Added debug logs

--- src/DfE.ExternalApplications.Api/Security/Handlers/ApplicationPermissionHandler.cs
+++ src/DfE.ExternalApplications.Api/Security/Handlers/ApplicationPermissionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
 using System.Security.Claims;
 
 namespace DfE.ExternalApplications.Api.Security.Handlers
@@ -6,13 +7,17 @@ namespace DfE.ExternalApplications.Api.Security.Handlers
     /// <summary>
     /// Authorization handler that checks user permission claims for a specific application resource.
     /// </summary>
-    public sealed class ApplicationPermissionHandler(IHttpContextAccessor accessor)
+    public sealed class ApplicationPermissionHandler(
+        IHttpContextAccessor accessor,
+        ILogger<ApplicationPermissionHandler> _logger)
         : AuthorizationHandler<ApplicationPermissionRequirement>
     {
         protected override Task HandleRequirementAsync(
             AuthorizationHandlerContext context,
             ApplicationPermissionRequirement requirement)
         {
+            _logger.LogWarning("ApplicationPermissionHandler > Entry");
+
             var applicationId = accessor.HttpContext?.Request.RouteValues["applicationId"]?.ToString();
             if (string.IsNullOrWhiteSpace(applicationId))
                 return Task.CompletedTask;
@@ -22,8 +27,20 @@ namespace DfE.ExternalApplications.Api.Security.Handlers
                 c.Type == "permission" &&
                 string.Equals(c.Value, expected, StringComparison.OrdinalIgnoreCase));
 
+            var claimStrings = context.User.Claims
+                .OrderBy(c => c.Type)
+                .Select(c => $"{c.Type}:{c.Value}")
+                .ToList();
+
+            _logger.LogWarning("ApplicationPermissionHandler > Expected: {expected}", expected);
+            _logger.LogWarning("ApplicationPermissionHandler > Claims : {claimStrings}", claimStrings);
+
+
             if (hasClaim)
+            {
+                _logger.LogWarning("ApplicationPermissionHandler > Has claim");
                 context.Succeed(requirement);
+            }
 
             return Task.CompletedTask;
         }
