ee037b5c4f9f9063d6792856cc672718558a4355
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-11-05 10:05:10 +0000

    Added EnableRetryOnFailure to the SQLServer connection

--- src/DfE.ExternalApplications.Infrastructure/InfrastructureServiceCollectionExtensions.cs
+++ src/DfE.ExternalApplications.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -32,7 +32,10 @@ namespace Microsoft.Extensions.DependencyInjection
             var connectionString = config.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<ExternalApplicationsContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlServer(connectionString, sql =>
+                {
+                    sql.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+                }));
 
             return services;
         }
